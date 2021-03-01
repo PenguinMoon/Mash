bcd696496299fd59832fda3a7cc17f4724ef7401
Luna <36724380+MoonBoop@users.noreply.github.com>
2021-03-01 11:34:29 +1100

    implemented a basic game manager, going to get a font for the gui next

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int _soldiersPickedUp = 0;
+    public static int _soldiersInHelicopter = 0;
+
+    private void Update()
+    {
+        
+    }
+
+    public static void DropOffSoldiers()
+    {
+        _soldiersPickedUp += _soldiersInHelicopter;
+        _soldiersInHelicopter = 0;
+    }
+
+    public static void GameOver()
+    {
+
+    }
+
+    public static void GameWon()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interactables/Hospital.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hospital : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void DropOffSoldier()
+    {
+        GameManager.DropOffSoldiers();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
