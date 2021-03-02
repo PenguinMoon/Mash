c0b879e5cbd1c1b7817395fa127e6cac5d458d50
Luna <36724380+MoonBoop@users.noreply.github.com>
2021-03-02 14:21:00 +1100

    Created a main menu scene and a main menu manager

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Starting game");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Opening settings");
+    }
+
+    public void CloseSettings()
+    {
+        Debug.Log("Close settings");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game");
+        Application.Quit();
+    }
+}
